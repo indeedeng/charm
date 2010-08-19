271daa45acacf912912a866508622e53e4a8e539
Jack Humphrey <jack@indeed.com>
2010-08-19 18:45:07 +0000

    copyright and GPL notices

--- src/main/java/com/indeed/charm/actions/DisplayLogVisitor.java
+++ src/main/java/com/indeed/charm/actions/DisplayLogVisitor.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2010 Indeed Inc.
+ *
+ * This file is part of CHARM.
+ *
+ * CHARM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CHARM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CHARM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.indeed.charm.actions;
 
 import com.indeed.charm.model.LogEntry;
